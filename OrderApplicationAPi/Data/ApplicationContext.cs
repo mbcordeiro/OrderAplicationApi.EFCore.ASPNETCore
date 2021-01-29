0fe1265267a889713109120ad5fca3044d59c62a
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-01-28 23:55:02 -0300

    config db context

--- OrderApplicationAPi/Data/ApplicationContext.cs
+++ OrderApplicationAPi/Data/ApplicationContext.cs
@@ -26,6 +26,23 @@ namespace OrderApplicationAPi.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+            MapForgottenProps(modelBuilder);
+        }
+
+        private void MapForgottenProps(ModelBuilder modelBuilder)
+        {
+            foreach(var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entity.GetProperties().Where(p => p.ClrType == typeof(string);
+                foreach(var property in properties)
+                {
+                    if(string.IsNullOrEmpty(property.GetColumnType()) && !property.GetMaxLength().HasValue) 
+                    {
+                        /*property.SetMaxLength(100);*/
+                        property.SetColumnType("VARCHAR(100)");
+                    }
+                }
+            }
         }
     }
 }
